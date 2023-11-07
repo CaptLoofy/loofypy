94882a56cea1242b4c71e40a5990df49fbc47a7f
CaptLoofy <sumaygupta7@gmail.com>
2023-11-06 23:33:55 -0800

    This is a simple program that prints a message, number and string.

---
+++ SimplePrint.py
@@ -0,0 +1,8 @@
+print("Testing Simple Print Statements")
+#initialize number
+age = 20
+#initialize a string
+name = "George"
+print("Testing printing a string and a number")
+print("Name",name)
+print("Age", age)
